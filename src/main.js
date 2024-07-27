e82fb8a819ec5f1e03d3e20aa79028d61cb5bc57
WSParker <will.parker0@gmail.com>
2024-07-27 16:01:39 -0700

    removed SW and added CNAME

--- src/main.js
+++ src/main.js
@@ -2,10 +2,6 @@ import "./app.css";
 import App from "./App.svelte";
 import { registerSW } from "virtual:pwa-register";
 
-if ("serviceWorker" in navigator) {
-    registerSW();
-}
-
 const app = new App({
     target: document.body,
 });
